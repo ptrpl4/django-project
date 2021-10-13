322607052679298a9a2ed4b088ed5955578aef21
Petr <ptrpl4@ya.ru>
2021-10-13 15:38:53 +0300

    added dummy data to test templates work

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+example_posts = [
+    {
+        'author': 'admin',
+        'title': 'The beginning',
+        'content': 'post content',
+        'date_posted': ' Today'
+    },
+{
+        'author': 'user',
+        'title': 'The ending',
+        'content': 'post content',
+        'date_posted': ' Yesterday'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': example_posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
