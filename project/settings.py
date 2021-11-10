a4b3c974ce573fa853f793aa475a8cdf2efb9727
Petr <ptrpl4@ya.ru>
2021-11-10 19:18:42 +0300

    added security note

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+# todo: hide secret key
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-aklz)$5*1tj64^=gbnd2(cr2#4vddkv0gtd#2%3kp%+=avusru'
 
